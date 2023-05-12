ca3e911a8898d147416851eab3fa67cb7b9312a0
Nicholasampah <113062261+Nicholasampah@users.noreply.github.com>
2023-05-12 14:29:16 +0100

    main.py

--- main.py
+++ main.py
@@ -20,52 +20,75 @@ cursor.execute(tables.inventory_table)
 cursor.execute("PRAGMA foreign_keys = 1")
 
 
-# Create the "created_at" trigger
-cursor.execute("""
-    CREATE TRIGGER IF NOT EXISTS inventory_created_at_trigger
-    AFTER INSERT ON inventory
-    BEGIN
-        UPDATE inventory SET created_at = datetime('now') WHERE id = NEW.id;
-    END;
-""")
 
-# Create the "updated_at" trigger
-cursor.execute("""
-    CREATE TRIGGER IF NOT EXISTS inventory_updated_at_trigger
-    AFTER UPDATE ON inventory
-    BEGIN
-        UPDATE inventory SET updated_at = datetime('now') WHERE id = NEW.id;
-    END;
-""")
+
+
+
+        
+    
+
 
 def customer_menu():
     customer_choice = ""
-    while customer_choice != "4":
+    while customer_choice != "5":
         print("""
         1. Make a booking
         2. View a booking
-        3. View boucing Castles
-        4. Previous Menu
+        3. Cancel booking
+        4. View boucing Castles
+        5. Previous Menu
         """)
         customer_choice = input("Please select an option: ")
-        if customer_choice == '4':
+        if customer_choice == '5':
             continue
         if customer_choice == "1":
             inventory_query = " SELECT * FROM inventory"
             print(pd.read_sql_query(inventory_query,connection))
         
-        customer_id= input("Enter your customer id ")
-        id= input("Enter the id for the product you want to hire ")
-        quantity= input("Enter the quantity ")
-        date_of_event= input("When is the event ")
-        number_of_days=input("How many days do you want hire ")
+            customer_id= input("Enter your customer id ")
+            id= input("Enter the id for the product you want to hire ")
+            quantity= input("Enter the quantity ")
+            date_of_event= input("When is the event ")
+            number_of_days=input("How many days do you want hire ")
         
-        check_inventory= "SELECT * FROM inventory WHERE id=?" 
-        cursor.execute(check_inventory, (id, )).fetchall()
-        add_order= f"INSERT INTO orders (customer_id, inventory_id, quantity, date_of_event, number_of_days) VALUES (?,?,?,?,?)" 
-        cursor.execute(add_order, ( customer_id, id, quantity, date_of_event, number_of_days))
-        connection.commit()
-        print("order made successfully.")
+            check_inventory= "SELECT * FROM inventory WHERE id=?" 
+            cursor.execute(check_inventory, (id, )).fetchall()
+            add_order= ("""INSERT INTO orders (customer_id, inventory_id, quantity, date_of_event, number_of_days, total, status) VALUES (?,?,?,?,?, 100, "AWAITING DISPATCH")""")
+            cursor.execute(add_order, ( customer_id, id, quantity, date_of_event, number_of_days,))
+            connection.commit()
+            print("order made successfully.")
+        
+        elif customer_choice == "2":
+            customer_id= input("Enter your customer number ")
+            value_order=cursor.execute("SELECT * FROM orders WHERE customer_id=?",(customer_id, )).fetchall()
+            print(value_order)
+        
+        elif customer_choice == "3":
+            customer_id = input("Enter your customer number: ")
+            order_id = input("Enter your order ID: ")
+            
+            query = "SELECT * FROM orders WHERE customer_id=? AND id=?"
+            value_order = cursor.execute(query, (customer_id, order_id)).fetchall()
+            print(value_order)
+            
+            print("Are you sure you want to delete this order? (y/n)")
+            del_choice = input("Answer y/n: ")
+            
+            if del_choice == "y":
+                delete_query = "DELETE FROM orders WHERE customer_id=? AND id=?"
+                cursor.execute(delete_query, (customer_id, order_id))
+                connection.commit()
+                print("Order deleted successfully.")
+            else:
+                continue
+        
+        elif customer_choice == "4":
+            inventory_query = " SELECT * FROM inventory"
+            print(pd.read_sql_query(inventory_query,connection))
+        
+        else:
+            print("Customer not found")
+            continue
 
      
 
@@ -112,82 +135,7 @@ def choice():
 choice()
    
         
-#menu_choice()
-# Call the function to create the trigger
-#create_delivery_trigger()
 
-
-##################INVENTORY TABLE#####################
-# # Insert data into INVENTORY TABLE
-# cursor.execute("""INSERT INTO inventory (id, name, quantity, price, colour, image, description, Status)  VALUES (101,"Princess Bouncy Castle with Slide", 6, 70, "Pink", "image1.jpg", "Contents: Bouncer, air blower, 8 stakes, carry bag, repair kit and manual
-# Electric air blower for continuous inflation and airflow
-# Set up in minutes for hours of fun
-# Mesh sides for adult supervision
-# Stakes anchor the bouncer firmly in place
-# Total weight limit: 113kg (max. 5 children at a time)
-# Dimensions: 295L x 233W x 175Hcm", "Avialable")""")
-# #View data into CUSTOMERS TABLE
-# inventory_query = " SELECT * FROM inventory"
-# print(pd.read_sql_query(inventory_query,connection))
-# # # Update data into INVENTORY TABLE
-# # # Delete data into INVENTORY TABLE
-
-# ##################VEHICLETYPE TABLE#####################
-# #insert data into VEHICLETYPE TABLE
-# cursor.execute("""INSERT INTO vehicletypes (id, type) VALUES(201, "Mini-Truck")""")
-# cursor.execute("""INSERT INTO vehicletypes (id, type) VALUES(202, "Truck")""")
-# cursor.execute("""INSERT INTO vehicletypes (id, type) VALUES(203, "Mini-Van")""")
-# cursor.execute("""INSERT INTO vehicletypes (id, type) VALUES(204, "Van")""")
-# #view data in VEHICLETYPE TABLE
-# vehicletype_query = " SELECT * FROM vehicletypes"
-# print(pd.read_sql_query(vehicletype_query,connection))
-# #Update data in vehicletypes
-# #Delete data in vehicle types
-# cursor.execute("""DELETE FROM vehicletypes WHERE""")
-
-
-
-# # ##################LOGISTICS TABLE#####################
-# # #Insert data into LOGISTIC TABLE
-# cursor.execute("""INSERT INTO logistics (licence_number, vehicle_type_id, vehicle_model, vehicle_year, vehicle_make, vehicle_colour, max_weight) VALUES("PC1 HRE", 201, "Sprinter", 2017, "Mercedes-Benz", "White", "1248kg")""")
-# # View data in LOgistics TABLE
-# logistics_query = " SELECT * FROM logistics"
-# print(pd.read_sql_query(logistics_query,connection))
-
-
-# # ##################DRIVERS TABLE#####################
-# # #Insert data into DRIVERS TABLE
-# cursor.execute("""INSERT INTO drivers (first_name, last_name, license_number, gender, dob, nationality, email, phone, address) VALUES ("Michael", "Kane", "KANEM874682A45T", "Male", "23-NOV-1969", "English", "kanem@gmail.com", 0759712507, "789 Market Street, Manchester" )""")
-# #view data in DRIVERS TABLE
-# driver_query = " SELECT * FROM drivers"
-# print(pd.read_sql_query(driver_query,connection))
-
-# # ##################CUSTOMERS TABLE#####################
-# #Insert data into CUSTOMERS TABLE
-# cursor.execute("""INSERT INTO customers (first_name, last_name, email, phone, address, postcode) VALUES("Nicholas", "Ampah", "nicholas.ampah@yahoo.com", 07860987621, "20 Oak Road, Manchester", "M99 0YU")""")
-# # View data into CUSTOMERS TABLE
-# customer_query = " SELECT * FROM customers"
-# print(pd.read_sql_query(customer_query,connection))
-# # # Update data into CUSTOMERS TABLE
-# cursor.execute("""UPDATE customers SET first_name="Judith", last_name="Brookes" , email="brookesme@aol.com", phone=07296397101, address="23 Travis Court, Manchester", postcode="M11 4TC" WHERE id=503""")
-# # # Delete data into CUSTOMERS TABLE
-# cursor.execute("""DELETE FROM customers WHERE id=505""")
-
-# # ##################ORDERS TABLE#####################
-# # #Insert data into ORDERS TABLE
-# cursor.execute("""INSERT INTO orders (customer_id, inventory_id, quantity, date_of_event, number_of_days, total, status) VALUES (501, 101, 2, "20-MAY-2023", 1, 100, "Awaiting Delivery")""")
-# #view data in ORDERS TABLE
-# orders_query = "SELECT * FROM orders"
-# print(pd.read_sql_query(orders_query,connection))
-
-# # ##################DELIVERY TABLE#####################
-# # #Insert data into DELIVERY TABLE
-# cursor.execute("""INSERT INTO deliveries (driver_id, order_id, status, deliver_by, complete_by, collect_at) VALUES (401, 601, "NOT DISPACHED", "19-MAY-2023", "20-MAY-2023", "20-MAY-2023")""")
-# #view data in DELIVERY TABLE
-# delivery_query = "SELECT * FROM deliveries"
-# print(pd.read_sql_query(delivery_query,connection))
-
-# cursor.execute("SELECT * FROM orders WHERE customer_id = 501 ")
 
 connection.commit()
 connection.close()
